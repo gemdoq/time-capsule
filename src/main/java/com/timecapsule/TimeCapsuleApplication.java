286120542971d2500457b8de11ed167e232c2b2c
SeungHo <gemdoq@gmail.com>
2026-02-01 20:22:57 +0900

    feat(init): 프로젝트 초기 설정
    Spring Boot 3.2.2 기반의 TimeCapsule 프로젝트 초기 설정입니다.
    
    - TimeCapsuleApplication.java: Spring Boot 메인 클래스
      @SpringBootApplication 어노테이션으로 자동 설정 및 컴포넌트 스캔 활성화
    
    - settings.gradle: Gradle 프로젝트 이름 설정 (time-capsule)
    
    - Gradle Wrapper: 프로젝트별 Gradle 버전 관리
      gradle-wrapper.jar: Wrapper 실행을 위한 JAR 파일
      gradle-wrapper.properties: Gradle 8.5 버전 지정
      gradlew/gradlew.bat: Unix/Windows용 Wrapper 스크립트
    
    Gradle Wrapper를 사용하면 로컬에 Gradle을 설치하지 않아도
    ./gradlew 명령으로 빌드를 실행할 수 있습니다.

---
+++ src/main/java/com/timecapsule/TimeCapsuleApplication.java
@@ -0,0 +1,14 @@
+package com.timecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TimeCapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeCapsuleApplication.class, args);
+    }
+}
