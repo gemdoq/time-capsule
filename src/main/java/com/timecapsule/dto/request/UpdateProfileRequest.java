18be8c77c5e5258d58a658ddc7701fe36ba2b0b5
SeungHo <gemdoq@gmail.com>
2026-02-01 20:25:23 +0900

    feat(dto): UpdateProfileRequest DTO 추가
    프로필 수정 요청 데이터를 담는 DTO입니다.
    
    필드 구성:
    - nickname: 변경할 닉네임 (@NotBlank)
    
    현재는 닉네임만 수정 가능하며,
    향후 프로필 이미지, 소개글 등 확장 가능합니다.
    
    프로필 수정 흐름:
    1. 클라이언트가 수정할 닉네임 전송
    2. JWT 토큰으로 현재 사용자 식별
    3. 사용자 정보 업데이트
    4. 업데이트된 UserResponse 반환

---
+++ src/main/java/com/timecapsule/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,15 @@
+package com.timecapsule.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateProfileRequest {
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다.")
+    private String nickname;
+}
